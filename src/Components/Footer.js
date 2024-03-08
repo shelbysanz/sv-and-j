c557f4583765efae8ac2272f039f3ede7db1fef0
Shelby Sanchez <shelby@bluearrowmail.com>
2024-03-08 15:22:49 -0500

    style images on mobile

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,15 +10,15 @@ class Footer extends Component {
     // );
     
 
-    const networks = this.props.data.social.map(function (network) {
-      return (
-         <li key={network.name}>
-          <a href={network.url}>
-            <i className={network.className}></i>
-          </a>
-        </li>
-      );
-    });
+    // const networks = this.props.data.social.map(function (network) {
+    //   return (
+    //      <li key={network.name}>
+    //       <a href={network.url}>
+    //         <i className={network.className}></i>
+    //       </a>
+    //     </li>
+    //   );
+    // });
 
     return (
       <footer>
